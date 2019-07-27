d471aa39d011697b7d8083b6caf8ef61ea24465a
Raul Jordan <raul@prysmaticlabs.com>
2019-07-27 16:31:57 -0500

    wrap up state getters and putters

--- beacon-chain/db/store/state.go
+++ beacon-chain/db/store/state.go
@@ -1,6 +1,9 @@
 package store
 
 import (
+	"errors"
+	"fmt"
+
 	"github.com/boltdb/bolt"
 	"github.com/prysmaticlabs/go-ssz"
 	pb "github.com/prysmaticlabs/prysm/proto/beacon/p2p/v1"
@@ -9,10 +12,42 @@ import (
 
 // CanonicalState retrieves the latest, canonical state from the db.
 func (b *BeaconDB) CanonicalState() (*pb.BeaconState, error) {
+	return b.getState(stateBucket, canonicalStateKey)
+}
+
+// FinalizedState retrieves the finalized state from the db.
+func (b *BeaconDB) FinalizedState() (*pb.BeaconState, error) {
+	return b.getState(stateBucket, finalizedStateKey)
+}
+
+// JustifiedState retrieves the justified state from the db.
+func (b *BeaconDB) JustifiedState() (*pb.BeaconState, error) {
+	return b.getState(stateBucket, justifiedStateKey)
+}
+
+// SaveCanonicalState saves the latest, canonical state to the db.
+func (b *BeaconDB) SaveCanonicalState(beaconState *pb.BeaconState) error {
+	return b.put(stateBucket, canonicalStateKey, beaconState)
+}
+
+// SaveFinalizedState saves the finalized state to the db.
+func (b *BeaconDB) SaveFinalizedState(beaconState *pb.BeaconState) error {
+	return b.put(stateBucket, finalizedStateKey, beaconState)
+}
+
+// SaveJustifiedState saves the justified state to the db.
+func (b *BeaconDB) SaveJustifiedState(beaconState *pb.BeaconState) error {
+	return b.put(stateBucket, justifiedStateKey, beaconState)
+}
+
+func (b *BeaconDB) getState(bucket []byte, key []byte) (*pb.BeaconState, error) {
 	var beaconState *pb.BeaconState
 	if err := b.db.View(func(tx *bolt.Tx) error {
-		bkt := tx.Bucket(stateBucket)
-		enc := bkt.Get(canonicalStateKey)
+		bkt := tx.Bucket(bucket)
+		enc := bkt.Get(key)
+		if enc == nil {
+			return fmt.Errorf("no item found for key: %s", key)
+		}
         return ssz.Unmarshal(enc, beaconState)
 	}); err != nil {
 		return nil, err
@@ -20,65 +55,13 @@ func (b *BeaconDB) CanonicalState() (*pb.BeaconState, error) {
 	return beaconState, nil
 }
 
-// FinalizedState retrieves the finalized state from the db.
-func (b *BeaconDB) FinalizedState() (*pb.BeaconState, error) {
-	var beaconState *pb.BeaconState
-	if err := b.db.View(func(tx *bolt.Tx) error {
-		bkt := tx.Bucket(stateBucket)
-		enc := bkt.Get(finalizedStateKey)
-		return ssz.Unmarshal(enc, beaconState)
-	}); err != nil {
-		return nil, err
-	}
-	return beaconState, nil
-}
-
-// JustifiedState retrieves the justified state from the db.
-func (b *BeaconDB) JustifiedState() (*pb.BeaconState, error) {
-	var beaconState *pb.BeaconState
-	if err := b.db.View(func(tx *bolt.Tx) error {
-		bkt := tx.Bucket(stateBucket)
-		enc := bkt.Get(finalizedStateKey)
-		return ssz.Unmarshal(enc, beaconState)
-	}); err != nil {
-		return nil, err
-	}
-	return beaconState, nil
-}
-
-// SaveCanonicalState saves the latest, canonical state to the db.
-func (b *BeaconDB) SaveCanonicalState(beaconState *pb.BeaconState) error {
+func (b *BeaconDB) put(bucket []byte, key []byte, item interface{}) error {
 	return b.db.Update(func(tx *bolt.Tx) error {
-		bkt := tx.Bucket(stateBucket)
-		enc, err := ssz.Marshal(beaconState)
+		bkt := tx.Bucket(bucket)
+		enc, err := ssz.Marshal(item)
 		if err != nil {
 			return err
 		}
-		return bkt.Put(canonicalStateKey, enc)
+		return bkt.Put(key, enc)
 	})
 }
-
-// SaveFinalizedState saves the finalized state to the db.
-func (b *BeaconDB) SaveFinalizedState(beaconState *pb.BeaconState) error {
-	return b.db.Update(func(tx *bolt.Tx) error {
-		bkt := tx.Bucket(stateBucket)
-		enc, err := ssz.Marshal(beaconState)
-		if err != nil {
-			return err
-		}
-		return bkt.Put(finalizedStateKey, enc)
-	})
-}
-
-// SaveJustifiedState saves the justified state to the db.
-func (b *BeaconDB) SaveJustifiedState(beaconState *pb.BeaconState) error {
-	return b.db.Update(func(tx *bolt.Tx) error {
-		bkt := tx.Bucket(stateBucket)
-		enc, err := ssz.Marshal(beaconState)
-		if err != nil {
-			return err
-		}
-		return bkt.Put(justifiedStateKey, enc)
-	})
-}
-
